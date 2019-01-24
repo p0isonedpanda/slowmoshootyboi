aa943a2433096fdd9c703e2a21b780c96b4cae34
Daniel Coady <daniel.coady@outlook.com.au>
2019-01-24 11:15:29 +1100

    Added animation to indicate when the player is ready to fire again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed = 5.0f;
-	public float fireRate = 0.1f;
 	public UnityEvent Moving, Standing;
+
+	public float speed = 5.0f;
 	[SerializeField]
 	GameObject projectile, blood;
+
+    // To fire the stuffs
+	public float fireRate = 0.1f;
 	float fireTimer = 0.0f;
+	bool ready;
+
 	Rigidbody2D rb;
+	Animator anim;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 
 		if (Moving == null)
 		    Moving = new UnityEvent();
@@ -34,11 +41,12 @@ public class PlayerController : MonoBehaviour
 		else Standing.Invoke();
 
         fireTimer += Time.deltaTime;
+		if (fireTimer >= fireRate) anim.SetBool("ready", true);
 
 		if (Input.GetButton("Fire1") && fireTimer >= fireRate)
 		{
 			fireTimer = 0.0f;
-			Moving.Invoke();
+			anim.SetBool("ready", false);
 			Instantiate(projectile, transform.position, transform.rotation);
 		}
 	}
